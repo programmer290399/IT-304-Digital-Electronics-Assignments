3e7e0f1ce9b67444f6302968221a79b8e6f6ef4d
programmer290399 <programmer290399@gmail.com>
2019-11-15 11:40:31 +0530

    PEP8 followed

--- Number-system-conversion-and-Calculator/converter.py
+++ Number-system-conversion-and-Calculator/converter.py
@@ -61,14 +61,7 @@ class NumberSystemConverter:
         self.input_base = input_base
         self.input_num = input_num
         self.target_base = target_base
-        self.hex_table = {
-                         10: 'A',
-                         11: 'B',
-                         12: 'C',
-                         13: 'D',
-                         14: 'E',
-                         15: 'F',
-                        }
+        self.hex_table = {10: "A", 11: "B", 12: "C", 13: "D", 14: "E", 15: "F"}
 
     def convert(self):
         """
@@ -80,7 +73,9 @@ class NumberSystemConverter:
 
             # converting number to decimal base
             digits = [int(digit) for digit in str(self.input_num)]
-            decimal_equivalent = sum([digit*(self.input_base**i) for i, digit in enumerate(digits[::-1])])
+            decimal_equivalent = sum(
+                [digit * (self.input_base ** i) for i, digit in enumerate(digits[::-1])]
+            )
 
             # converting the obtained decimal_equivalent to the desired base
             target_num = list()
@@ -88,10 +83,17 @@ class NumberSystemConverter:
             while True:
 
                 if decimal_equivalent == 0:
-                    if self.target_base == 16 :
-                        target_num = ''.join(map(lambda num: str(self.hex_table[num]) if num in range(10, 16) else str(num), target_num[::-1]))
+                    if self.target_base == 16:
+                        target_num = "".join(
+                            map(
+                                lambda num: str(self.hex_table[num])
+                                if num in range(10, 16)
+                                else str(num),
+                                target_num[::-1],
+                            )
+                        )
                     else:
-                        target_num = ''.join(map(str, target_num[::-1]))
+                        target_num = "".join(map(str, target_num[::-1]))
                     return target_num
 
                 current_remainder = decimal_equivalent % self.target_base
@@ -101,11 +103,23 @@ class NumberSystemConverter:
 
         else:
 
-            int_digits, frac_digits = map(lambda part: [int(digit) for digit in part], str(self.input_num).split('.'))
-            decimal_equivalent_int, decimal_equivalent_frac = map(lambda lst: sum(lst), [
-                                                                                            [digit * (self.input_base ** i) for i, digit in enumerate(int_digits[::-1])],
-                                                                                            [digit*(self.input_base ** (-1 * (i+1))) for i, digit in enumerate(frac_digits)],
-                                                                                        ])
+            int_digits, frac_digits = map(
+                lambda part: [int(digit) for digit in part],
+                str(self.input_num).split("."),
+            )
+            decimal_equivalent_int, decimal_equivalent_frac = map(
+                lambda lst: sum(lst),
+                [
+                    [
+                        digit * (self.input_base ** i)
+                        for i, digit in enumerate(int_digits[::-1])
+                    ],
+                    [
+                        digit * (self.input_base ** (-1 * (i + 1)))
+                        for i, digit in enumerate(frac_digits)
+                    ],
+                ],
+            )
 
             # Converting integer part to desired base
             target_num_int = list()
@@ -114,9 +128,16 @@ class NumberSystemConverter:
 
                 if decimal_equivalent_int == 0:
                     if self.target_base == 16:
-                        target_num_int = ''.join(map(lambda num: str(self.hex_table[num]) if num in range(10, 16) else str(num), target_num_int[::-1]))
+                        target_num_int = "".join(
+                            map(
+                                lambda num: str(self.hex_table[num])
+                                if num in range(10, 16)
+                                else str(num),
+                                target_num_int[::-1],
+                            )
+                        )
                     else:
-                        target_num_int = ''.join(map(str, target_num_int[::-1]))
+                        target_num_int = "".join(map(str, target_num_int[::-1]))
                     break
 
                 current_remainder = decimal_equivalent_int % self.target_base
@@ -129,11 +150,20 @@ class NumberSystemConverter:
             iteration_count = 0
             while True:
 
-                if any([iteration_count >= 10, float(decimal_equivalent_frac).is_integer()]):
+                if any(
+                    [iteration_count >= 10, float(decimal_equivalent_frac).is_integer()]
+                ):
                     if self.target_base == 16:
-                        target_num_frac = ''.join(map(lambda num: str(self.hex_table[num]) if num in range(10, 16) else str(num), target_num_frac))
+                        target_num_frac = "".join(
+                            map(
+                                lambda num: str(self.hex_table[num])
+                                if num in range(10, 16)
+                                else str(num),
+                                target_num_frac,
+                            )
+                        )
                     else:
-                        target_num_frac = ''.join(map(str, target_num_frac))
+                        target_num_frac = "".join(map(str, target_num_frac))
                     break
 
                 decimal_equivalent_frac *= self.target_base
@@ -142,9 +172,9 @@ class NumberSystemConverter:
                 decimal_equivalent_frac = fraction
                 iteration_count += 1
 
-            return target_num_int + '.' + target_num_frac
+            return target_num_int + "." + target_num_frac
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     test = NumberSystemConverter(8, 25.37, 2)
     print(test.convert())
